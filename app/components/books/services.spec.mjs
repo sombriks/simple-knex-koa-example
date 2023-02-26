73752d73003175fa2345dceb6a40457eec962996
sombriks <sombriks@gmail.com>
2023-02-26 15:37:45 -0300

    update tests checks

--- app/components/books/services.spec.mjs
+++ app/components/books/services.spec.mjs
@@ -7,13 +7,22 @@ chai.should();
 
 describe("Books service test", () => {
 
+  // setup database for testing
   before(async () => await doMigrate())
   after(async () => await knex.destroy())
 
-  it("should list books by name", async () => {
+  it("should list all books", async () => {
     const books = await listBooks("")
 
     books.should.be.an("array")
+    books.should.have.lengthOf(3)
+  })
+
+  it("should filter books by name", async () => {
+    const books = await listBooks("neuro")
+
+    books.should.be.an("array")
+    books.should.have.lengthOf(1)
   })
 
 })
